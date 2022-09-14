35f7c16379d566b75178b6e44cc4a22ca90628f5
prince9871 <soniprince142@gmail.com>
2022-09-14 15:03:25 +0530

    project done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,29 +1,29 @@
-const collegeModel=require('../model/collegeModel')
-const internModel=require('../model/internModel')
-const createCollege=async function(req,res){
-    try{
-    let collegeData=await collegeModel.create(req.body)
-    res.status(201).send({status:true,msg:'College Data Created Successfully',collegeData})
-        }
-catch (err) { 
-    res.status(500).send({ status: false, msg: err.msg }) 
-}
+const collegeModel = require('../model/collegeModel')
+const internModel = require('../model/internModel')
+const createCollege = async function (req, res) {
+    try {
+        let collegeData = await collegeModel.create(req.body)
+        res.status(201).send({ status: true, msg: 'College Data Created Successfully', collegeData })
+    }
+    catch (err) {
+        res.status(500).send({ status: false, msg: err.msg })
+    }
 }
 
-const collegeDetails=async function(req,res){
-    try{
-   let query = req.query
-    
-    let collegeData=await collegeModel.find(...query)
-    console.log(collegeData._id)
-    let internData=await internModel.find({collegeId:collegeData._id})
-    console.log(internData)
-    let Data={name:collegeData.name,fullName:collegeData.fullName,logolink:collegeData.logoLink,interns:internData}
-    res.status(200).send({status:true,msg:"Data Found",Data:Data})
+const collegeDetails = async function (req, res) {
+    try {
+        let { collegeName } = req.query
+
+        let collegeData = await collegeModel.findOne({ name: collegeName })
+
+        let internData = await internModel.find({ collegeId: collegeData._id })
+
+        let Data = { name: collegeData.name, fullName: collegeData.fullName, logolink: collegeData.logoLink, interns: internData }
+        res.status(200).send({ status: true, msg: "Data Found", Data: Data })
     }
-    catch (error) { 
-        res.status(500).send({ status: false, msg: error.msg }) 
+    catch (error) {
+        res.status(500).send({ status: false, msg: error.msg })
     }
 }
-module.exports.createCollege=createCollege
-module.exports.collegeDetails=collegeDetails
+module.exports.createCollege = createCollege
+module.exports.collegeDetails = collegeDetails
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -6,16 +6,15 @@ const isValidMobileNumber=function(number){
 }
 
 const createIntern = async function (req, res) {
-    // try {
+    try {
         let {mobile} = req.body
-        
         if(!isValidMobileNumber(mobile)) return res.status(400).send({status:false,msg:'Enter Valid Mobile Number'})
         let internData = await internModel.create(req.body)
         res.status(201).send({ status: true, msg: "Intern Data Created Successfully", internData })
     }
-    // catch (error) { 
-        // res.status(500).send({ status: false, msg: error.msg }) 
-    // }
-
+    catch (error) { 
+        res.status(500).send({ status: false, msg: error.msg }) 
+    }
+}
 
 module.exports.createIntern=createIntern
