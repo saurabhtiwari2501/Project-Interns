150ddb8a53140246b7b69668ffe738c61f778d14
Manish-choubey <109017549+Manish-choubey@users.noreply.github.com>
2022-09-14 11:22:13 +0530

    ok

--- src/model/internModel.js
+++ src/model/internModel.js
@@ -4,7 +4,7 @@ const ObjectId=mongoose.Schema.Types.ObjectId
 const internSchema = new mongoose.Schema({
         name: {type:String,required:true},
         email: {type:String,unique:true},
-        mobile: {type:Number,unique:true,required:true},
+        mobile: {type:String,unique:true,required:true},
         collegeId: {type:ObjectId,required:true,ref:'CollegeDB'},
         isDeleted:{type:Boolean,default: false }
 }, { timestamps: true })
--- src/route/route.js
+++ src/route/route.js
@@ -1,5 +1,5 @@
 const express=require('express')
-const router=express.Router
+const router=express.Router()
 const collegeController=require('../controller/collegeController')
 const internController=require('../controller/internController')
 
