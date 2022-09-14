ce98007e75848ad4588096ea6f28da19200cd149
prince9871 <soniprince142@gmail.com>
2022-09-14 12:35:37 +0530

    ok

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -5,4 +5,11 @@ const createCollege=async function(req,res){
     res.status(201).send({status:true,msg:'College Data Created Successfully',collegeData})
 }
 
+const collegeDetails=async function(req,res){
+    let collegeName=req.query.collegeName
+    //let collegeData=await collegeModel.findOne(collegeName)
+    let internData=await collegeModel.find().populate('collegeId')
+    res.send(internData)
+    
+}
 module.exports.createCollege=createCollege
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,8 +1,14 @@
 const internModel = require('../model/internModel')
 
+//validation function are here
+const isValidMobileNumber=function(number){
+    return /^(\+\d{1,3}[- ]?)?\d{10}$/.test(number)
+}
+
 const createIntern = async function (req, res) {
     try {
         let internData = await internModel.create(req.body)
+        if(!isValidMobileNumber(mobile)) return res.status(400).send({status:false,msg:'Enter Valid Mobile Number'})
         res.status(201).send({ status: true, msg: "Intern Data Created Successfully", internData })
     }
     catch (err) { 
