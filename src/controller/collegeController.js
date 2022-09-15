076b476c6aa3e36e90ee3724dd706e4a475e7d3c
prince9871 <soniprince142@gmail.com>
2022-09-15 19:06:21 +0530

    ok

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -11,15 +11,16 @@ const createCollege = async function (req, res) {
         if (!v.isValidSpace(name)) return res.status(400).send({ status: false, msg: 'name is required' })
         if (!v.isNameLower(name)) return res.status(400).send({ status: false, msg: 'name lower case or - allowed only' })
 
-        let checkDuplicate = await collegeModel.findOne({ name: name })
-        if (checkDuplicate) return res.status(400).send({ status: false, msg: 'Please Enter Another Data-name already exist' })
+         
+        if (await collegeModel.findOne({ name: name })) return res.status(400).send({ status: false, msg: 'Please Enter Another Data-name already exist' })
 
         if (!v.isValidSpace(fullName)) return res.status(400).send({ status: false, msg: 'FullName is required' })
         if (!v.isValidfullName(fullName)) return res.status(400).send({ status: false, msg: 'FullName Allowed Only In Alphabetic Form' })
         if (!v.isValidSpace(logoLink)) return res.status(400).send({ status: false, msg: 'logolink is required' })
         if (!v.isValidLink(logoLink)) return res.status(400).send({ status: false, msg: 'logolink is required in valid format' })
-        let deletedStatus = isDeleted
-        if (deletedStatus) return res.status(400).send({ status: false, msg: 'Data Creation Failed' })
+        
+        
+        if (isDeleted) return res.status(400).send({ status: false, msg: 'Data Creation Failed' })
         let collegeData = await collegeModel.create(requestBody)
         res.status(201).send({ status: true, data: 'College Data Created Successfully', collegeData })
     }
--- src/controller/internController.js
+++ src/controller/internController.js
@@ -1,29 +1,37 @@
 const internModel = require('../model/internModel')
+const collegeModel = require('../model/collegeModel')
 const v=require('../validation/validation')
 
 const createIntern = async function (req, res) {
     try {
         let requestBody=req.body
         if(!v.isvalidRequest(requestBody)) return res.status(400).send({status:false,msg:'body data is required'})
-        let {name,email,mobile,collegeId} = requestBody
+        
+        let {name,email,mobile,collegeName} = requestBody
+
         if(!v.isValidSpace(name)) return res.status(400).send({status:false,msg:'name is required'})
         if(!v.isValidName(name)) return res.status(400).send({status:false,msg:'name is required in valid format'})
+        
         if(!v.isValidSpace(email)) return res.status(400).send({status:false,msg:'email is required'})
         if(!v.isValidEmail(email)) return res.status(400).send({status:false,msg:'email is required in valid format'})
+        
         let checkDuplicateEmail=await internModel.findOne({email:email}) 
         if(checkDuplicateEmail) return res.status(400).send({status:false,msg:'Please Enter Another Data-email already exist'})
         
         if(!v.isValidSpace(mobile)) return res.status(400).send({status:false,msg:'mobile is required'})
-        if(!v.isValidMobileNumber(mobile)) return res.status(400).send({status:false,msg:'enter 10 digit valid mobile number with +91'})
+        if(!v.isValidMobileNumber(mobile)) return res.status(400).send({status:false,msg:'enter 10 diggiit valid mobile number with +91'})
+        
         let checkDuplicateMobile=await internModel.findOne({mobile:mobile}) 
         if(checkDuplicateMobile) return res.status(400).send({status:false,msg:'Please Enter Another Data-mobile already exist'})
         
-        if(!v.isValidSpace(collegeId)) return res.status(400).send({status:false,msg:'collegeId is required'})
-        if(!v.isValidObjectId(collegeId)) return res.status(400).send({status:false,msg:'collegeId is required in valid format'})
-        // let checkcollegeId=await internModel.findOne({collegeId:collegeId}) 
-        // if(checkcollegeId) return res.status(400).send({status:false,msg:'Please Enter Another Data-mobile already exist'})
-        
-        let internData = await internModel.create(req.body)
+        if(!v.isValidSpace(collegeName)) return res.status(400).send({status:false,msg:'collegeId is required'})
+        if (!v.isNameLower(collegeName)) return res.status(400).send({ status: false, msg: 'collegeName lower case or - allowed only' })
+
+        let Data=await collegeModel.findOne({name:collegeName})
+        if(!Data) return res.status(400).send({status:false,msg:'Enter Valid collegeName'})
+        let collegeId=Data._id
+        let DataBlock={name,email,mobile,collegeId}
+        let internData = await internModel.create(DataBlock)
         res.status(201).send({ status: true, msg: "Intern Data Created Successfully", internData })
     }
     catch (error) { 
